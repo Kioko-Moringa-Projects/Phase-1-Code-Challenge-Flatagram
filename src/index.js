d1403d5ec1060adfb56aab98a70a069f8ad103cc
Calvin <calvinkioko91@gmail.com>
2022-06-20 00:23:48 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,63 @@
-// write your code here
+const imageUrl = 'http://localhost:3000/images/'
+const commentsUrl = 'http://localhost:3000/comments/'
+
+const imageGoesHere = document.getElementById('card-image')
+const titleGoesHere = document.getElementById('card-title')
+const commentsGoHere = document.getElementById('comments-list')
+const likeButton = document.getElementById('like-button')
+const likesNum = document.getElementById('like-count')
+const commentContainer = document.getElementById('comment')
+
+
+fetch(imageUrl)
+    .then(resp => resp.json())
+    // .then((json) => console.log(json))
+    .then(iterateImagesArray)
+    .catch(console.error)
+
+fetch(commentsUrl)
+    .then(resp => resp.json())
+    // .then((json) => console.log(json))
+    .then(iterateCommentsArray)
+    .catch(console.error)
+
+
+function iterateImagesArray(json) {
+    json.forEach(addImageToPage)
+}
+function iterateCommentsArray(json) {
+    json.forEach(addCommentsToPage)
+}
+
+function addImageToPage(imageObj) {
+    const title = titleGoesHere;
+    title.textContent = imageObj.title
+
+    const imgElem = imageGoesHere;
+    imgElem.src = imageObj.image
+    imgElem.id = imageObj.id
+    likeButton.addEventListener('click', () => {
+        ++imageObj.likes
+        likesNum.textContent = imageObj.likes
+    })
+}
+
+function addCommentsToPage(commentsObj) {
+    console.log(commentsObj)
+    const realComments = commentsGoHere;
+    realComments.id = commentsObj.id
+    realComments.textContent = commentsObj.content
+}
+
+
+
+
+commentContainer.addEventListener('submit', addNewComment)
+
+function addNewComment(event) {
+    event.preventDefault()
+    const newComment = event.target.content.value;
+    addNewComment(newComment)
+    commentContainer.reset()
+
+}
